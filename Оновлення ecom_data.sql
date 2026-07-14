7ee6a6d325478c80f803dc92b4d4bc693ef0aa94
MixFora <m.papanov@fora.ua>
2026-07-14 12:40:09 +0300

    Add files via upload

--- Оновлення ecom_data.sql
+++ Оновлення ecom_data.sql
@@ -3,7 +3,7 @@
     SELECT *,
            ROW_NUMBER() OVER (PARTITION BY ordernum ORDER BY type) AS rn
     FROM Business_Analytic.ecom.ecom_orders
-	where year=2026 and month=5
+	where year=2026 and month=6
 )
 select * FROM Duplicates WHERE rn > 1;
 --select * into #dubs FROM Duplicates WHERE rn > 1;
@@ -19,7 +19,7 @@ join #delete d on d.ordernum=eo.ordernum and d.year=eo.year and d.month=eo.month
 
 /*SELECT ordernumber, ordersum, createdate, chequeid, filialid, city, street
   FROM [DBMS_CulinaryOrder].[CulinaryOrder].[Export].[view_Orders]
-  where ordernumber in (24663440, 24663491, 24664596)*/
+  where ordernumber=24710208 in (24663440, 24663491, 24664596)*/
 
 
 drop table if exists #final
@@ -27,15 +27,15 @@ SELECT e.ordernum, v.createdate, v.chequeid, e.type, e.year, e.month, v.filialid
 into #final
 FROM Business_Analytic.ecom.ecom_orders e
 join [DBMS_CulinaryOrder].[CulinaryOrder].[Export].[view_Orders] v on e.ordernum=v.ordernumber
-and e.year=2026 and e.month=5 and (e.chequeid=0 or e.filid=0 or e.chequeid is NULL) 
-and v.createdate BETWEEN '2026-04-25' AND '2026-06-05' --and e.type='Glovo'
+and e.year=2026 and e.month=6 and (e.chequeid=0 or e.filid=0 or e.chequeid is NULL) 
+and v.createdate BETWEEN '2026-05-25' AND '2026-07-05' --and e.type='Glovo'
 
 select * from #final
 order by chequeid
 
 select * from #final f
 join Business_Analytic.ecom.ecom_orders e on e.ordernum=f.ordernum
-where  e.year=2026 and e.month=5 and (e.chequeid=0 or e.chequeid is null)
+where  e.year=2026 and e.month=6 and (e.chequeid=0 or e.chequeid is null)
 order by f.chequeid 
 	
 
@@ -51,14 +51,8 @@ update eo
 set eo.chequeid=f.chequeid, eo.filid=f.filid
 from Business_Analytic.ecom.ecom_orders eo
 join #final f on eo.ordernum=f.ordernum
-where eo.year=2026 and eo.month=5 and f.chequeid is not null and f.chequeid <>0
+where eo.year=2026 and eo.month=6 and f.chequeid is not null and f.chequeid <>0
 
-drop table if exists #misses
-select eo.*, f.createdate, e.CHEQUEID cheq, e.filid fil, e.type typee into #misses from Business_Analytic.ecom.ecom_orders eo 
-join #final f on eo.ordernum=f.ordernum
-left join (select distinct chequeid, filid, type from Business_Analytic.ecom.ecom_data
-where  created BETWEEN '2026-03-01' and '2026-04-05') e on eo.chequeid=e.chequeid and eo.filid=e.filid
-where eo.year=2026 and eo.month=3 and e.chequeid is null
 
 --select * from #misses
 
@@ -69,27 +63,9 @@ SET ed.[type] = e.[type]
 FROM Business_Analytic.ecom.ecom_data ed
 INNER JOIN Business_Analytic.ecom.ecom_orders e
     ON ed.chequeid = e.chequeid and ed.filid = e.filid
-WHERE ed.created BETWEEN '2026-04-25' and '2026-06-05'
-    AND e.month = 5 AND e.year=2026
+WHERE ed.created BETWEEN '2026-05-25' and '2026-07-05'
+    AND e.month = 6 AND e.year=2026
     AND ed.[type] = 'Доставка shop.fora.ua'
 	--and e.type='Рой'
     AND e.[type] IS NOT NULL
 	and e.ChequeID is not null
-
-/*
-select type, count(distinct chequeid) [к-сть чеків, вересень] from Business_Analytic.ecom.ecom_data
-where created BETWEEN '2026-05-01' and '2026-05-31' --and type in ('ipost', 'uklon')
-group by type
-
-select type, count(distinct chequeid) [к-сть чеків, серпень] from Business_Analytic.ecom.ecom_data
-where created BETWEEN '2025-12-01' and '2025-12-31' --and type in ('ipost', 'uklon')
-group by type
-*/
-
-select distinct chequeid, filid [к-сть чеків, вересень] from Business_Analytic.ecom.ecom_data
-where created BETWEEN '2026-05-01' and '2026-05-31' and type not in ('Самовывоз shop.fora.ua', 'Glovo')
-group by chequeid, filid
-
-select distinct chequeid [к-сть чеків, вересень] from Business_Analytic.ecom.ecom_data
-where created BETWEEN '2026-05-01' and '2026-05-31' and type not in ('Самовывоз shop.fora.ua', 'Glovo')
-group by chequeid
